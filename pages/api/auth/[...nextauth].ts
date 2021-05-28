cb6c65002ed8a773ae6be8ac3db69fcee1dac731
louismeunier <louismeunier123@gmail.com>
2021-05-28 14:35:59 -0400

    Auth formatting

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,15 +8,15 @@ export default NextAuth({
 		// OAuth authentication providers...
 		Providers.Google({
 			clientId: process.env.GOOGLE_ID,
-			clientSecret: process.env.GOOGLE_SECRET,
-		}),
+			clientSecret: process.env.GOOGLE_SECRET
+		})
 	],
 	callbacks: {
 		async signIn(user, account, profile) {
 			createUser({ name: user.name, email: user.email });
 			return true;
-		},
-	},
+		}
+	}
 	// Optional SQL or MongoDB database to persist users
 	//database: process.env.DATABASE_URL
 });
