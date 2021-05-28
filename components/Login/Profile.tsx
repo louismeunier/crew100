4482b5cafe00a7e9d655cc68d57bac1237aa321e
louismeunier <louismeunier123@gmail.com>
2021-05-28 14:34:03 -0400

    Get user from db on sign in

--- components/Login/Profile.tsx
+++ components/Login/Profile.tsx
@@ -3,18 +3,20 @@ import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
-import { useSession, signOut } from "next-auth/client";
+import { signOut, getSession } from "next-auth/client";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		display: "flex",
-	},
+		display: "flex"
+	}
 }));
 
 export default function Profile() {
+	const [session, setSession] = React.useState(null);
+	const [user, setUser] = React.useState(null);
 	const classes = useStyles();
-	const [session, loading] = useSession();
+
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -25,30 +27,41 @@ export default function Profile() {
 		setAnchorEl(null);
 	};
 
+	React.useEffect(() => {
+		getSession().then(sess => {
+			setSession(sess);
+			fetch(`http://localhost:3000/api/user?name=${sess.user.name}`)
+				.then(user => user.json())
+				.then(userJson => {
+					setUser(userJson);
+				});
+		});
+	}, []);
+
 	return (
-		<div>
-			<Button
-				aria-controls="simple-menu"
-				aria-haspopup="true"
-				onClick={handleClick}
-			>
-				Profile
-				<KeyboardArrowDownIcon />
-			</Button>
-			<Menu
-				className={classes.root}
-				id="simple-menu"
-				anchorEl={anchorEl}
-				keepMounted
-				open={Boolean(anchorEl)}
-				onClose={handleClose}
-			>
-				{session && !loading && (
-					<MenuItem onClick={handleClose}>{session.user.name}</MenuItem>
-				)}
-				<MenuItem onClick={handleClose}>My account</MenuItem>
-				<MenuItem onClick={() => signOut()}>Logout</MenuItem>
-			</Menu>
-		</div>
+		session && (
+			<div>
+				<Button
+					aria-controls="simple-menu"
+					aria-haspopup="true"
+					onClick={handleClick}
+				>
+					{session && session.user.name}
+					<KeyboardArrowDownIcon />
+				</Button>
+				<Menu
+					className={classes.root}
+					id="simple-menu"
+					anchorEl={anchorEl}
+					keepMounted
+					open={Boolean(anchorEl)}
+					onClose={handleClose}
+				>
+					{user && <MenuItem>{user.role}</MenuItem>}
+					<MenuItem onClick={handleClose}>My account</MenuItem>
+					<MenuItem onClick={() => signOut()}>Logout</MenuItem>
+				</Menu>
+			</div>
+		)
 	);
 }
