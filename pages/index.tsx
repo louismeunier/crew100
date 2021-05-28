fc0dfea169bad6adcbdddd46e59f01b68d03d3e0
louismeunier <louismeunier123@gmail.com>
2021-05-28 14:39:31 -0400

    Add title to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,49 @@ import Grid from "@material-ui/core/Grid";
 import HomeMainBar from "@components/Home/HomeMainBar";
 import { makeStyles } from "@material-ui/core/styles";
 import Carousel from "@components/Home/Carousel";
-import InfoBar from "@components/Home/InfoBar"
+import InfoBar from "@components/Home/InfoBar";
+import Head from "next/head";
 
 const useStyles = makeStyles(theme => ({
 	container: {
 		paddingRight: theme.spacing(2),
-		paddingLeft: theme.spacing(2),
+		paddingLeft: theme.spacing(2)
 	},
 	carouselWrapper: {
-		width: "100%"
+		width: "100%",
+		marginBottom: theme.spacing(16)
 	},
 	infoWrapper: {
 		width: "50%"
 	}
 }));
+
 const HomePage: NextPage = () => {
 	const classes = useStyles();
 
 	return (
-		<Grid 
-			container 
-			className={classes.container}
-			direction="column"
-            justify="center"
-            alignItems="center"
-		>
-			<Grid item>
-				<HomeMainBar />
+		<>
+			<Head>
+				<title>Crew 100 | Home</title>
+			</Head>
+			<Grid
+				container
+				className={classes.container}
+				direction="column"
+				justify="center"
+				alignItems="center"
+			>
+				<Grid item>
+					<HomeMainBar />
+				</Grid>
+				<Grid item className={classes.infoWrapper}>
+					<InfoBar />
+				</Grid>
+				<Grid item className={classes.carouselWrapper}>
+					<Carousel />
+				</Grid>
 			</Grid>
-			<Grid item className={classes.infoWrapper}>
-				<InfoBar/>
-			</Grid>
-			<Grid item className={classes.carouselWrapper}>
-				<Carousel/>
-			</Grid>
-		</Grid>
+		</>
 	);
 };
 
